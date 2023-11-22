4548773ba9d673e67dae3709ffdc67473ef5e0ea
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-11-22 17:46:13 +0100

    edit navbar - set icons

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,10 +1,18 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <router-link to="/home" class="navbar-brand">Home</router-link>
-      <router-link to="/bmi" class="navbar-brand">BMI</router-link>
-      <router-link to="/diary" class="navbar-brand">Diary</router-link>
-      <router-link to="/profile" class="navbar-brand">Profile</router-link>
+      <router-link to="/home" class="navbar-brand">
+        <span class="mdi mdi-home"></span>
+      </router-link>
+      <router-link to="/bmi" class="navbar-brand">
+        <span class="mdi mdi-chart-box"></span>
+      </router-link>
+      <router-link to="/diary" class="navbar-brand">
+        <span class="mdi mdi-application-edit-outline"></span>
+      </router-link>
+      <router-link to="/profile" class="navbar-brand">
+        <span class="mdi mdi-account"></span>
+      </router-link>
     </div>
   </nav>
 </template>
@@ -17,6 +25,14 @@ export default {
 
 <style scoped>
 .navbar {
+  background-color: #d29433;
+}
+
+.navbar-brand {
+  font-size: 28px;
+}
+
+.mdi:hover {
   background-color: #d29433;
 }
 </style>
