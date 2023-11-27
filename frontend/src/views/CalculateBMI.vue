09a7602f0baae358b205ae0a23f8571d36c54d47
Luka Benković <benkovi.luka3@gmail.com>
2023-11-27 21:22:48 +0100

    napravio novi izgled za back button, i novi izgled za navbar

--- frontend/src/views/CalculateBMI.vue
+++ frontend/src/views/CalculateBMI.vue
@@ -1,208 +1,213 @@
 <template>
-  <div>
-    <div class="main" style="background-color: black; margin: 0">
-      <div class="header" style="padding-left: 15px">
-        <router-link to="/" class="btn btn-dark" style="width: 50px">
-          <span class="material-symbols-outlined"> arrow_back_ios </span>
-        </router-link>
-      </div>
-      <div class="gender">
-        <h1 class="naslov">BMI CALCULATOR</h1>
-        <button
-          @click="maleChoose"
-          :class="{ 'btn-primary': male, 'btn-secondary': !male }"
-          :style="maleBtnStyle"
-          size="lg"
-        >
-          <i class="bi bi-gender-male" style="font-size: 2rem"></i>
-        </button>
-        <button
-          @click="femaleChoose"
-          :class="{ 'btn-primary': female, 'btn-secondary': !female }"
-          :style="femaleBtnStyle"
-          size="lg"
-        >
-          <i class="bi bi-gender-female" style="font-size: 2rem"></i>
-        </button>
-      </div>
-      <div class="bmi">
-        <div class="input">
-          <input
-            style="background-color: #d29433"
-            class="form-control height"
-            v-model="height"
-            type="number"
-            placeholder="Height (cm)"
-          />
-          <input
-            style="background-color: #d29433"
-            class="form-control weight"
-            v-model="weight"
-            type="number"
-            placeholder="Weight (kg)"
-          />
-          <input
-            style="background-color: #d29433"
-            class="form-control age"
-            v-model="age"
-            type="number"
-            placeholder="Age"
-          />
-        </div>
+	<div>
+		<div class="main" style="background-color: black; margin: 0">
+			<div class="header" style="padding-left: 15px">
+				<router-link to="/" class="btn-dark" style="width: 50px">
+					<span class="btn-dark material-symbols-outlined">
+						arrow_back_ios
+					</span>
+				</router-link>
+			</div>
+			<div class="gender">
+				<h1 class="naslov">BMI CALCULATOR</h1>
+				<button
+					@click="maleChoose"
+					:class="{ 'btn-primary': male, 'btn-secondary': !male }"
+					:style="maleBtnStyle"
+					size="lg">
+					<i class="bi bi-gender-male" style="font-size: 2rem"></i>
+				</button>
+				<button
+					@click="femaleChoose"
+					:class="{ 'btn-primary': female, 'btn-secondary': !female }"
+					:style="femaleBtnStyle"
+					size="lg">
+					<i class="bi bi-gender-female" style="font-size: 2rem"></i>
+				</button>
+			</div>
+			<div class="bmi">
+				<div class="input">
+					<input
+						style="background-color: #d29433"
+						class="form-control height"
+						v-model="height"
+						type="number"
+						placeholder="Height (cm)" />
+					<input
+						style="background-color: #d29433"
+						class="form-control weight"
+						v-model="weight"
+						type="number"
+						placeholder="Weight (kg)" />
+					<input
+						style="background-color: #d29433"
+						class="form-control age"
+						v-model="age"
+						type="number"
+						placeholder="Age" />
+				</div>
 
-        <button class="btn btn-success" @click="calculateBMI">Calculate</button>
+				<button class="btn btn-success" @click="calculateBMI">
+					Calculate
+				</button>
 
-        <div v-if="bmi != null">
-          <h2 class="result">Your BMI: {{ bmi }}</h2>
-          <p class="message">{{ bmiMessage }}</p>
-        </div>
-      </div>
-    </div>
-    <nav-bar class="navbar" />
-  </div>
+				<div v-if="bmi != null">
+					<h2 class="result">Your BMI: {{ bmi }}</h2>
+					<p class="message">{{ bmiMessage }}</p>
+				</div>
+			</div>
+		</div>
+		<nav-bar class="navbar" />
+	</div>
 </template>
 
 <script>
 import NavBar from "../components/NavBar.vue";
 
 export default {
-  name: "CalculateBMI",
-  components: {
-    NavBar,
-  },
-  data() {
-    return {
-      height: null,
-      weight: null,
-      age: null,
-      bmi: null,
-      male: false,
-      female: false,
+	name: "CalculateBMI",
+	components: {
+		NavBar,
+	},
+	data() {
+		return {
+			height: null,
+			weight: null,
+			age: null,
+			bmi: null,
+			male: false,
+			female: false,
 
-      maleBtnStyle: {
-        height: "100px",
-        width: "100px",
-        margin: "10px 43px 10px 43px",
-        borderRadius: "20px",
-        backgroundColor: "#D29433",
-      },
-      femaleBtnStyle: {
-        height: "100px",
-        width: "100px",
-        margin: "10px 43px 10px 43px",
-        borderRadius: "20px",
-        backgroundColor: "#D29433",
-      },
-    };
-  },
-  methods: {
-    calculateBMI() {
-      if (this.height && this.weight) {
-        const heightInMeters = this.height / 100;
-        this.bmi = (this.weight / (heightInMeters * heightInMeters)).toFixed(2);
-      } else {
-        this.bmi = null;
-      }
-    },
-    maleChoose() {
-      this.male = !this.male;
-      this.female = false;
-      this.maleBtnStyle.backgroundColor = this.male ? "cyan" : "#D29433";
-      this.femaleBtnStyle.backgroundColor = "#D29433";
-    },
-    femaleChoose() {
-      this.female = !this.female;
-      this.male = false;
-      this.femaleBtnStyle.backgroundColor = this.female ? "pink" : "#D29433";
-      this.maleBtnStyle.backgroundColor = "#D29433";
-    },
-  },
+			maleBtnStyle: {
+				height: "100px",
+				width: "100px",
+				margin: "10px 43px 10px 43px",
+				borderRadius: "20px",
+				backgroundColor: "#D29433",
+			},
+			femaleBtnStyle: {
+				height: "100px",
+				width: "100px",
+				margin: "10px 43px 10px 43px",
+				borderRadius: "20px",
+				backgroundColor: "#D29433",
+			},
+		};
+	},
+	methods: {
+		calculateBMI() {
+			if (this.height && this.weight) {
+				const heightInMeters = this.height / 100;
+				this.bmi = (
+					this.weight /
+					(heightInMeters * heightInMeters)
+				).toFixed(2);
+			} else {
+				this.bmi = null;
+			}
+		},
+		maleChoose() {
+			this.male = !this.male;
+			this.female = false;
+			this.maleBtnStyle.backgroundColor = this.male ? "cyan" : "#D29433";
+			this.femaleBtnStyle.backgroundColor = "#D29433";
+		},
+		femaleChoose() {
+			this.female = !this.female;
+			this.male = false;
+			this.femaleBtnStyle.backgroundColor = this.female
+				? "pink"
+				: "#D29433";
+			this.maleBtnStyle.backgroundColor = "#D29433";
+		},
+	},
 
-  computed: {
-    bmiMessage() {
-      if (this.bmi === null) {
-        return "";
-      } else if (this.bmi < 18.5) {
-        return "You are undernourished. Consult your doctor to increase body weight.";
-      } else if (this.bmi > 24.9) {
-        return "You have excess body weight. Consult a doctor to reduce it.";
-      } else {
-        return "You have a normal body weight. Eat healthily and engage in physical activity to maintain it.";
-      }
-    },
-  },
+	computed: {
+		bmiMessage() {
+			if (this.bmi === null) {
+				return "";
+			} else if (this.bmi < 18.5) {
+				return "You are undernourished. Consult your doctor to increase body weight.";
+			} else if (this.bmi > 24.9) {
+				return "You have excess body weight. Consult a doctor to reduce it.";
+			} else {
+				return "You have a normal body weight. Eat healthily and engage in physical activity to maintain it.";
+			}
+		},
+	},
 };
 </script>
 
 <style>
+@import "../stylesheet.css";
 .gender {
-  text-align: center;
+	text-align: center;
 }
 
 .bmi {
-  margin: auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+	margin: auto;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 }
 
 .result {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: white;
-  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
-    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	color: white;
+	font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+		"Lucida Sans Unicode", Geneva, Verdana, sans-serif;
 }
 
 .input {
-  margin: 0;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+	margin: 0;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 }
 
 .height,
 .weight,
 .age {
-  background-color: #d29433;
-  max-width: 300px;
-  padding: 20px;
-  border-radius: 20px;
-  text-align: center;
-  margin: 5px;
-  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
-    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
-  font-size: medium;
+	background-color: #d29433;
+	max-width: 300px;
+	padding: 20px;
+	border-radius: 20px;
+	text-align: center;
+	margin: 5px;
+	font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+		"Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+	font-size: medium;
 }
 
 .btn {
-  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
-    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
-  padding: 10px;
-  margin: 20px;
+	font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+		"Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+	padding: 10px;
+	margin: 20px;
 }
 
 .naslov {
-  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
-    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
-  color: aliceblue;
+	font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+		"Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+	color: aliceblue;
 }
 
 .message {
-  margin-left: 21px;
-  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
-    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
-  color: rgb(222, 174, 43);
+	margin-left: 21px;
+	font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+		"Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+	color: rgb(222, 174, 43);
 }
 
 .navbar {
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  text-align: center;
+	position: fixed;
+	bottom: 0;
+	width: 100%;
+	text-align: center;
 }
 </style>
--- frontend/src/views/LogIn.vue
+++ frontend/src/views/LogIn.vue
@@ -1,87 +1,102 @@
 <template>
-  <div class="main">
-    <div class="header" style="padding-left: 15px">
-      <router-link to="/" class="btn btn-dark" style="width: 100px">
-        <span class="material-symbols-outlined"> arrow_back_ios </span>
-      </router-link>
-    </div>
-    <h2 style="color: white; text-align: center">Log in to your account</h2>
-    <div class="input-login">
-      <h3 style="color: white">E-mail</h3>
-      <input
-        type="email"
-        class="form-control input-field"
-        style="background-color: #d29433"
-      />
-      <h3 style="color: white">Password</h3>
-      <input
-        type="password"
-        class="form-control input-field"
-        style="background-color: #d29433"
-      />
-      <div class="submit">
-        <button type="button" class="btn btn-success" :style="btnstyleGreen">
-          <span
-            v-if="loading"
-            class="spinner-border spinner-border-sm"
-            role="status"
-            aria-hidden="true"
-          ></span>
-          <span v-else>Login</span>
-        </button>
-        <button type="button" class="btn btn-secondary" :style="btnstyleGray">
-          Cancel
-        </button>
-      </div>
-    </div>
-  </div>
+	<div class="main">
+		<div class="header" style="padding-left: 15px">
+			<router-link to="/" class="btn-dark" style="width: 100px">
+				<span class="btn-dark material-symbols-outlined">
+					arrow_back_ios
+				</span>
+			</router-link>
+		</div>
+		<h2 style="color: white; text-align: center">Log in to your account</h2>
+		<div class="input-login">
+			<h3 style="color: white">E-mail</h3>
+			<input
+				type="email"
+				class="form-control input-field"
+				style="background-color: #d29433" />
+			<h3 style="color: white">Password</h3>
+			<input
+				type="password"
+				class="form-control input-field"
+				style="background-color: #d29433" />
+			<div class="submit">
+				<button
+					type="button"
+					class="btn btn-success"
+					:style="btnstyleGreen">
+					<span
+						v-if="loading"
+						class="spinner-border spinner-border-sm"
+						role="status"
+						aria-hidden="true"></span>
+					<span v-else>Login</span>
+				</button>
+				<button
+					type="button"
+					class="btn btn-secondary"
+					:style="btnstyleGray">
+					Cancel
+				</button>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "LogIn",
-  data() {
-    return {
-      btnstyleGreen: {
-        borderRadius: "20px",
-        width: "300px",
-      },
-      btnstyleGray: {
-        borderRadius: "20px",
-        width: "300px",
-        marginTop: "10px",
-      },
-    };
-  },
+	name: "LogIn",
+	data() {
+		return {
+			btnstyleGreen: {
+				borderRadius: "20px",
+				width: "300px",
+			},
+			btnstyleGray: {
+				borderRadius: "20px",
+				width: "300px",
+				marginTop: "10px",
+			},
+		};
+	},
 };
 </script>
-<style>
+<style scoped>
+@import "../stylesheet.css";
+
 .input-login {
-  margin-top: 30px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+	margin-top: 30px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
 }
 .input-field {
-  background-color: #d29433;
-  width: 300px;
-  min-width: 100px;
-  padding: 8px 16px 8px 16px;
-  border-radius: 20px;
-  text-align: center;
-  margin: 10px;
+	background-color: #d29433;
+	width: 300px;
+	min-width: 100px;
+	padding: 8px 16px 8px 16px;
+	border-radius: 20px;
+	text-align: center;
+	margin: 10px;
 }
 .submit {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-top: 30px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	margin-top: 30px;
 }
 
-.btn-dark {
-  background-color: #d29433;
-  margin-top: 20px;
+/* .btn-dark {
+	background-color: black;
+	border: none;
+	margin: 20px 0 0 0 !important;
+	padding: 0 !important;
+}
+.btn-dark:hover {
+	background-color: black;
+} */
+.header {
+	margin: 0;
 }
 </style>
