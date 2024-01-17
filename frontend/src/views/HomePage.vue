d2fd26fea6f059a9c88729ab97120b406bd988a4
Mirna Pušeljić <puseljicmirna@gmail.com>
2024-01-17 15:46:35 +0100

    adding my-workouts

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -2,7 +2,13 @@
   <div class="heading">
     <img class="heading-img" src="@/assets/logorsg.jpg" alt="" />
   </div>
-  <div class="main" style="background-color: black; margin: 0">
+  <div
+    class="main"
+    :style="{
+      'background-color': 'black',
+      height: '200%',
+    }"
+  >
     <div class="header" style="padding-left: 15px"></div>
 
     <div class="header">
@@ -111,6 +117,101 @@
         <span class="visually-hidden">Next</span>
       </button>
     </div>
+
+    <!--my workouts-->
+    <div class="my-workout">
+      <h1 style="color: white">MY WORKOUTS</h1>
+    </div>
+    <div id="carouselExampleCaptionsMyWorkouts" class="carousel slide">
+      <div class="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptionsMyWorkouts"
+          data-bs-slide-to="0"
+          class="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptionsMyWorkouts"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptionsMyWorkouts"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img
+            src="../assets/crossfit.jpg"
+            class="d-block"
+            style="height: 60vh; width: auto"
+          />
+          <div class="carousel-caption d-none d-md-block">
+            <h3>CrossFit</h3>
+            <p>
+              Whether you’ve trained your whole life or are just starting your
+              fitness journey, CrossFit offers a results-based, community-driven
+              approach that helps you build fitness and improve your health—over
+              your lifetime.
+            </p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img
+            src="../assets/lower-body.jpg"
+            class="d-block"
+            style="height: 60vh; width: auto"
+          />
+          <div class="carousel-caption d-none d-md-block">
+            <h3>Lower Body Attack</h3>
+            <p>
+              Use these timeless leg exercises to gain mass and strength on your
+              lower body. A varied combination of reps and sets will help to
+              keep your routine fresh.
+            </p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img
+            src="../assets/upper-body.jpg"
+            class="d-block"
+            style="height: 60vh; width: auto"
+          />
+          <div class="carousel-caption d-none d-md-block">
+            <h3>Upper Body Attack</h3>
+            <p>
+              Time to get the arms, shoulders and chest into it! You will need
+              some dumbbells to get this one done.
+            </p>
+          </div>
+        </div>
+      </div>
+      <button
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleCaptionsMyWorkouts"
+        data-bs-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleCaptionsMyWorkouts"
+        data-bs-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+
     <nav-bar class="navbar" />
   </div>
 </template>
@@ -156,7 +257,8 @@ export default {
   border-radius: 50px;
 }
 
-.recommended-workout {
+.recommended-workout,
+.my-workout {
   margin: 50px;
 }
 .carousel-inner {
@@ -166,5 +268,13 @@ export default {
 }
 .carousel-item {
   height: 60vh;
+}
+
+.navbar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  z-index: 2;
 }
 </style>
