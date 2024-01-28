ece047f6bcc7dc49f5a2d219883cc83da65f69bc
Mirna Pušeljić <puseljicmirna@gmail.com>
2024-01-28 21:30:16 +0100

    napravljen modal za recommended workout

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,39 +1,34 @@
 <template>
-    <div class="heading">
-        <img
-            class="heading-img"
-            src="@/assets/logorsg.jpg"
-            alt=""
-            @click="eventBusTest(), toggleModal()"
-        />
+  <div class="heading">
+    <img class="heading-img" src="@/assets/logorsg.jpg" alt="" />
+  </div>
+  <div
+    class="main"
+    :style="{
+      'background-color': 'black',
+      height: '200%',
+    }"
+  >
+    <div class="header" style="padding-left: 15px"></div>
+
+    <div class="header">
+      <h1 style="color: #d29433">Let's crush it!</h1>
     </div>
-    <div
-        class="main"
-        :style="{
-            'background-color': 'black',
-            height: '200%',
-        }"
-    >
-        <div class="header" style="padding-left: 15px"></div>
 
-        <div class="header">
-            <h1 style="color: #d29433">Let's crush it!</h1>
-        </div>
+    <!--search-->
+    <div class="container mt-3">
+      <input
+        class="form-control"
+        placeholder="Search anything..."
+        style="background-color: #9a9494; color: black"
+      />
+    </div>
 
-        <!--search-->
-        <div class="container mt-3">
-            <input
-                class="form-control"
-                placeholder="Search anything..."
-                style="background-color: #9a9494; color: black"
-            />
-        </div>
-
-        <!--recommended workouts-->
-        <div class="recommended-workout">
-            <h1 style="color: white">RECOMMENDED WORKOUTS</h1>
-        </div>
-        <!-- <div id="carouselExampleCaptions" class="carousel">
+    <!--recommended workouts-->
+    <div class="recommended-workout">
+      <h1 style="color: white">RECOMMENDED WORKOUTS</h1>
+    </div>
+    <!-- <div id="carouselExampleCaptions" class="carousel">
             <div class="carousel-indicators">
                 <button
                     type="button"
@@ -129,77 +124,87 @@
                 <span class="visually-hidden">Next</span>
             </button>
         </div> -->
-        <v-container>
-            <v-carousel hide-delimiters>
-                <template v-slot:prev="{ props }">
-                    <v-btn @click="props.onClick" icon>
-                        <v-icon>mdi-chevron-left</v-icon>
-                    </v-btn>
-                </template>
-                <template v-slot:next="{ props }">
-                    <v-btn class="custom-next-btn" @click="props.onClick" icon>
-                        <v-icon>mdi-chevron-right</v-icon>
-                    </v-btn>
-                </template>
-                <v-carousel-item cover>
-                    <img class="carousel-image" src="../assets/crossfit.jpg" />
-                </v-carousel-item>
+    <v-container>
+      <v-carousel hide-delimiters>
+        <template v-slot:prev="{ props }">
+          <v-btn @click="props.onClick" icon>
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </template>
+        <template v-slot:next="{ props }">
+          <v-btn class="custom-next-btn" @click="props.onClick" icon>
+            <v-icon>mdi-chevron-right</v-icon>
+          </v-btn>
+        </template>
+        <v-carousel-item cover @click="openModal('CROSSFIT')">
+          <img class="carousel-image" src="../assets/crossfit.jpg" />
+          <div class="carousel-caption d-none d-md-block">
+            <h1>CROSSFIT</h1>
+            <p>
+              Whether you’ve trained your whole life or are just starting your
+              fitness journey,<br />
+              CrossFit offers a results-based, <br />community-driven approach
+              that helps you build fitness <br />
+              and improve your health—over your lifetime.
+            </p>
+          </div>
+        </v-carousel-item>
 
-                <v-carousel-item cover>
-                    <img
-                        class="carousel-image"
-                        src="../assets/lower-body.jpg"
-                    />
-                </v-carousel-item>
+        <v-carousel-item cover @click="openModal('LOWER-BODY')">
+          <img class="carousel-image" src="../assets/lower-body.jpg" />
+          <div class="carousel-caption d-none d-md-block">
+            <h1>LOWER BODY ATTACK</h1>
+            <p>
+              Use these timeless leg exercises to gain mass and strength on your
+              lower body. A varied combination of reps and sets will help to
+              keep your routine fresh.
+            </p>
+          </div>
+        </v-carousel-item>
 
-                <v-carousel-item cover>
-                    <img
-                        class="carousel-image"
-                        src="../assets/upper-body.jpg"
-                    />
-                </v-carousel-item>
-            </v-carousel>
-        </v-container>
+        <v-carousel-item cover @click="openModal('UPPER-BODY')">
+          <img class="carousel-image" src="../assets/upper-body.jpg" />
+          <div class="carousel-caption d-none d-md-block">
+            <h1>UPPER BODY ATTACK</h1>
+            <p>
+              Time to get the arms, shoulders and chest into it! <br />
+              You will need some dumbbells to get this one done.
+            </p>
+          </div>
+        </v-carousel-item>
+      </v-carousel>
+    </v-container>
 
-        <!--my workouts-->
-        <div class="my-workout">
-            <h1 style="color: white">MY WORKOUTS</h1>
-        </div>
-        <v-container>
-            <v-carousel hide-delimiters>
-                <template v-slot:prev="{ props }">
-                    <v-btn @click="props.onClick" icon>
-                        <v-icon>mdi-chevron-left</v-icon>
-                    </v-btn>
-                </template>
-                <template v-slot:next="{ props }">
-                    <v-btn class="custom-next-btn" @click="props.onClick" icon>
-                        <v-icon>mdi-chevron-right</v-icon>
-                    </v-btn>
-                </template>
-                <v-carousel-item cover>
-                    <img
-                        class="carousel-image"
-                        src="../assets/my-workout-first.jpg"
-                    />
-                </v-carousel-item>
+    <!--my workouts-->
+    <div class="my-workout">
+      <h1 style="color: white">MY WORKOUTS</h1>
+    </div>
+    <v-container>
+      <v-carousel hide-delimiters>
+        <template v-slot:prev="{ props }">
+          <v-btn @click="props.onClick" icon>
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </template>
+        <template v-slot:next="{ props }">
+          <v-btn class="custom-next-btn" @click="props.onClick" icon>
+            <v-icon>mdi-chevron-right</v-icon>
+          </v-btn>
+        </template>
+        <v-carousel-item cover>
+          <img class="carousel-image" src="../assets/my-workout-first.jpg" />
+        </v-carousel-item>
 
-                <v-carousel-item cover>
-                    <img
-                        class="carousel-image"
-                        src="../assets/my-workout-second.jpg"
-                    />
-                </v-carousel-item>
+        <v-carousel-item cover>
+          <img class="carousel-image" src="../assets/my-workout-second.jpg" />
+        </v-carousel-item>
 
-                <v-carousel-item cover>
-                    <img
-                        class="carousel-image"
-                        src="../assets/my-workout-third.jpg"
-                    />
-                </v-carousel-item>
-            </v-carousel>
-        </v-container>
-        <!-- <div id="carouselExampleCaptionsMyWorkouts" class="carousel">
+        <v-carousel-item cover>
+          <img class="carousel-image" src="../assets/my-workout-third.jpg" />
+        </v-carousel-item>
+      </v-carousel>
+    </v-container>
+    <!-- <div id="carouselExampleCaptionsMyWorkouts" class="carousel">
             <div class="carousel-indicators">
                 <button
                     type="button"
@@ -279,14 +284,15 @@
                 <span class="visually-hidden">Next</span>
             </button>
         </div> -->
-        <!--exercise list-->
-        <div class="exercise-list">
-            <h1 style="color: white">EXERCISE LIST</h1>
-        </div>
-
-        <!-- <nav-bar class="navbar" /> -->
-        <exercise-modal-body :active-modal="activeModal" />
+    <!--exercise list-->
+    <div class="exercise-list">
+      <h1 style="color: white">EXERCISE LIST</h1>
     </div>
+
+    <!-- <nav-bar class="navbar" /> -->
+    <exercise-modal-body :active-modal="activeModal" />
+    <div class="modal-overlay" v-if="activeModal" @click="closeModal"></div>
+  </div>
 </template>
 <script>
 /* eslint-disable */
@@ -295,92 +301,127 @@ import exerciseModalBody from "@/modals/exerciseModalBody.vue";
 // import NavBar from "@/components/NavBar.vue";
 
 export default {
-    name: "HomePage",
-    data() {
-        return {
-            activeModal: false,
-        };
+  name: "HomePage",
+  data() {
+    return {
+      activeModal: false,
+      clickedInsideModal: false,
+    };
+  },
+  components: {
+    // NavBar,
+    exerciseModalBody,
+  },
+  created() {
+    eventBus.on("closeModal", (closeModalData) => {
+      if (closeModalData.closeModal) this.activeModal = false;
+    });
+    eventBus.on("openModal", (modalData) => {
+      if (
+        modalData.workoutName === "CROSSFIT" ||
+        modalData.workoutName === "LOWER-BODY" ||
+        modalData.workoutName === "UPPER-BODY"
+      ) {
+        this.activeModal = true;
+      }
+    });
+  },
+  methods: {
+    eventBusTest() {
+      let id = 4;
+      eventBus.emit("test", id);
     },
-    components: {
-        // NavBar,
-        exerciseModalBody,
+    toggleModal() {
+      this.activeModal = !this.activeModal;
     },
-    created() {
-        eventBus.on("closeModal", (closeModalData) => {
-            if (closeModalData.closeModal) this.activeModal = false;
-        });
+    openModal(workoutName) {
+      this.clickedInsideModal = false;
+      this.activeModal = true;
+      eventBus.emit("openModal", { workoutName });
     },
-    methods: {
-        eventBusTest() {
-            let id = 4;
-            eventBus.emit("test", id);
-        },
-        toggleModal() {
-            this.activeModal = !this.activeModal;
-        },
+    closeModal() {
+      if (!this.clickedInsideModal) {
+        this.activeModal = false;
+      }
     },
+  },
 };
 </script>
 <style scoped>
 .mdi-chevron-right,
 .mdi-chevron-left {
-    color: black;
+  color: black;
 }
 .carousel-image {
-    width: 100%;
-    max-height: 100%;
-    object-fit: contain;
+  width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+  opacity: 0.7;
 }
 .heading {
-    margin: 0;
-    padding: 1vw 5vw 0 2vw;
-    height: 7vh;
-    background-color: #000;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  margin: 0;
+  padding: 1vw 5vw 0 2vw;
+  height: 7vh;
+  background-color: #000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .heading-img {
-    height: 100%;
-    width: auto;
-    object-fit: contain;
-    overflow: hidden;
+  height: 100%;
+  width: auto;
+  object-fit: contain;
+  overflow: hidden;
 }
 
 .header {
-    padding-top: 5px;
-    padding-left: 20px;
+  padding-top: 5px;
+  padding-left: 20px;
 }
 .container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .form-control {
-    border-radius: 50px;
+  border-radius: 50px;
 }
 
 .recommended-workout,
 .my-workout,
 .exercise-list {
-    margin: 50px;
+  margin: 50px;
 }
 .carousel-inner {
-    height: 60vh;
-    display: grid;
-    place-content: center;
+  height: 60vh;
+  display: grid;
+  place-content: center;
 }
 .carousel-item {
-    height: 60vh;
+  height: 60vh;
 }
 
 .navbar {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    z-index: 2;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  z-index: 2;
+}
+
+.carousel-caption {
+  color: #d29433;
+}
+
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 999;
 }
 </style>
